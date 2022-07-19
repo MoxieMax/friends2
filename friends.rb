e276154a4eb6d3959af6faf2b2f9b8425585397a
Max MacGillivray <mary.macgillivray@gmail.com>
2022-07-19 11:02:35 -0700

    Add method to print integers under 60

--- friends.rb
+++ friends.rb
@@ -1,7 +1,13 @@
 numbers = [87, 21, 7, 52, 77, 2, 18, 99]
-
+=begin
 numbers.each do |number|
   if number > 30
+    puts number
+  end
+end
+=end
+numbers.each do |number|
+  if number < 60
     puts number
   end
 end
